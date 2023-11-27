ec89d05e6979300706328070d3e375b76b33f82c
kwarnerredhat <kwarner@redhat.com>
2023-11-27 14:11:58 -0500

    Changed the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,9 +28,9 @@ export default function Header() {
 
   return (
     <PageHeader>
-      <PageHeaderTitle title='Custom Repositories' />
-      <Text className={classes.subtext} ouiaId='custom_repositories_description'>
-        View all custom repositories within your organization.
+      <PageHeaderTitle title='Repositories' />
+      <Text className={classes.subtext} ouiaId='repositories_description'>
+        View all repositories within your organization.
       </Text>
     </PageHeader>
   );
